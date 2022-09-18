d7edb6528035e65fcc15f33edb309b202f6233c4
Karan Janthe <86996507+KMJ-007@users.noreply.github.com>
2022-09-18 09:01:53 +0530

    added karan's sample bot

---
+++ karanBot.js
@@ -0,0 +1,72 @@
+// Supports ES6
+// import { create, Whatsapp } from 'venom-bot';
+const venom = require('venom-bot');
+
+venom
+  .create({
+    session: 'session-name', //name of session
+    multidevice: true // for version not multidevice use false.(default: true)
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage(async(message) => {
+    if (message.body === 'Hi' && message.isGroupMsg === false) {
+      
+        const list = [
+            {
+              title: "Pasta",
+              rows: [
+                {
+                  title: "Ravioli Lasagna",
+                  description: "Made with layers of frozen cheese",
+                }
+              ]
+            },
+            {
+              title: "Dessert",
+              rows: [
+                {
+                  title: "Baked Ricotta Cake",
+                  description: "Sweets pecan baklava rolls",
+                },
+                {
+                  title: "Lemon Meringue Pie",
+                  description: "Pastry filled with lemonand meringue.",
+                }
+              ]
+            }
+          ];
+          const buttons = [
+            {
+              "buttonText": {
+                "displayText": "Text of Button 1"
+                }
+              },
+            {
+              "buttonText": {
+                "displayText": "Text of Button 2"
+                }
+              }
+            ]
+        console.log("msg from",message.from)
+        // await client.sendListMenu(`9909912610@c.us`, 'Title', 'subTitle', 'Description', 'menu', list)
+        //   .then((result) => {
+        //     console.log('Result: ', result); //return object success
+        //   })
+        //   .catch((erro) => {
+        //     console.error('Error when sending: ', erro); //return object error
+        //   });
+        await client.sendButtons('9909912610@c.us', 'Title', buttons, 'Description')
+            .then((result) => {
+                console.log('Result: ', result); //return object success
+            })
+            .catch((erro) => {
+                console.error('Error when sending: ', erro); //return object error
+            });
+    }
+  });
+}
