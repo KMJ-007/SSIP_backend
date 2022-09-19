67a355504b925766c0004f16c0603676d89e6040
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2022-09-19 22:46:16 +0530

    Course/BE/Computer branch is ready in Bot.js and data for computer branch is added in Data/SelectionData.js

---
+++ Data/SelectionData.js
@@ -0,0 +1,94 @@
+
+const GTU = {
+    Course: {
+        BE: {
+            Computer_Engineering: {
+                one: {
+                    "Chemistry": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110002.pdf",
+                    "English": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110002.pdf",
+                    "Programming for Problem Solving": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110003.pdf",
+                    "Basic_Electrical_Engineering": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110005.pdf",
+                    "Basic Mechanical Engineeringg": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110006.pdf",
+                    "Environmental Sciences": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110007.pdf",
+                    "Workshop/ Manufacturing Practices": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110012.pdf",
+                    "Engineering Graphics & Design": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110013.pdf ",
+                    "Mathematics - 1": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110014.pdf",
+                    "Mathematics - 2": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110015.pdf",
+                    "Basic Electronics": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110016.pdf",
+                    "Induction Program": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110017.pdf",
+                    "Physics": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110018.pdf"
+
+                },
+                three: {
+                    "Effective Technical Communication": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3130004.pdf",
+                    "Probability and Statistics": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3130006.pdf",
+                    "Indian Constitution": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3130007.pdf",
+                    "Design Engineering - I A": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3130008.pdf",
+                    "Data Structures": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3130702.pdf",
+                    "Database Management Systems": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3130703.pdf",
+                    "Digital Fundamentals": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3130704.pdf"
+                },
+                four: {
+                    "Design Engineering 1 B": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3140005.pdf",
+                    "Operating System": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3140702.pdf",
+                    "Object Oriented Programming -I": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3140705.pdf",
+                    "Computer Organization & Architecture": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3140707.pdf",
+                    "Discrete Mathematics": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3140708.pdf",
+                    "Principles of Economics and Management": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3140709.pdf",
+
+                },
+                five: {
+                    "Contributor Personality Development Program": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3150004.pdf",
+                    "Integrated Personality Development Course": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3150005.pdf",
+                    "Analysis and Design of Algorithms": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3150703.pdf",
+                    "Professional ethics": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3150709.pdf",
+                    "Computer Networks": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3150710.pdf",
+                    "Software Engineering": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3150711.pdf",
+                    "Computer Graphics": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3150712.pdf",
+                    "Python for Data Science": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3150713.pdf",
+                    "Cyber Security": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3150714.pdf",
+                },
+                six: {
+                    "Contributor Personality Development Program": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3160002.pdf",
+                    "Integrated Personality Development Course": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3160003.pdf",
+                    "Theory of Computation": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3160704.pdf",
+                    "Advanced Java Programming": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3160707.pdf",
+                    "Microprocessor and Interfacing": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3160712.pdf",
+                    "Web Programming": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3160713.pdf",
+                    "Data Mining": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3160714.pdf",
+                    "System Software": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3160715.pdf",
+                    "IOT and applications": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3160716.pdf",
+                    "Data Visualization": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3160717.pdf",
+
+                },
+                seven: {
+                    "Summer Internship": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170001.pdf",
+                    "Complier Design": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170701.pdf",
+                    "Mobile Computing and Wireless communication": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170710.pdf",
+                    "Artificial Intelligence": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170716.pdf",
+                    "Cloud Computing": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170717.pdf",
+                    "Information Retrieval": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170718.pdf",
+                    "Distributed System": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170719.pdf",
+                    "Information security": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170720.pdf",
+                    "Parallel and Distributed Computing": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170721.pdf",
+                    "Big Data Analytics": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170722.pdf",
+                    "Natural Language Processing": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170723.pdf",
+                    "Machine Learning": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170724.pdf",
+                    "Digital forensics": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170725.pdf",
+                    "Mobile Application Development": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3170726.pdf",
+                },
+                eight: {
+                    "Internship/Project": "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3180701.pdf",
+
+                }
+
+            }
+
+        }
+
+
+    }
+}
+
+
+module.exports = GTU;
--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 // const venom = require('venom-bot');
 // const { ListenerLayer } = require('venom-bot/dist/api/layers/listener.layer');
-const GTU = require('./SelectionData');
+const GTU = require('./Data/SelectionData');
 const prompt = require('prompt-sync')({ sigint: true });
-// console.log(GTU.Course.BE.Computer_Engineering);
-
+let a = "Basic_Electrical_Engineering";
+console.log(GTU.Course.BE.Computer_Engineering.one[a]);
+// const mylist = require('./abc.js');
 // venom
 //     .create({
 //         session: 'Test-Session', //name of session
@@ -14,44 +15,61 @@ const prompt = require('prompt-sync')({ sigint: true });
 //         console.log(erro);
 //     });
 
-selector = (key) => {
-    let a;
-    a = prompt("Enter the key");
-    if (key[a] == undefined) {
-        console.log(key);
-        return key;
-    }
-    selector(key[a]);
-}
+// // selector = (key) => {
+// //     let a;
+// //     a = prompt("Enter the key");
+// //     if (key[a] == undefined) {
+// //         console.log(key);
+// //         return key;
+// //     }
+// //     selector(key[a]);
+// // }
 
-let NewKey = selector(GTU);
+// // let NewKey = selector(GTU);
 
-console.log(NewKey);
+// // console.log(NewKey);
+// client.onMessage((message) => {
+//     const List = [
+//         {
+//             title: "this is the title",
+//             rows: [
+//                 mylist
 
-// const course = {
+//             ]
 
-//     Chemistry: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110002.pdf",
-//     English: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110002.pdf",
-//     Programming_for_Problem_Solving: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110003.pdf",
-//     Basic_Electrical_Engineering: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110005.pdf",
-//     Basic_Mechanical_Engineering: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110006.pdf",
-//     Environmental_Sciences: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110007.pdf",
-//     Workshop_Manufacturing_Practices: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110012.pdf",
-//     Engineering_Graphics_Design: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110013.pdf ",
-//     Mathematics_1: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110014.pdf",
-//     Mathematics_2: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110015.pdf",
-//     Basic_Electronics: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110016.pdf",
-//     Induction_Program: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110017.pdf",
-//     Physics: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110018.pdf"
-
-
+//         }
+//     ]
+//     client.sendListMenu(message.from, '', '', 'Select the branch:', 'Select', list)
 // }
 
-// let courseFinder = (courseName) => {
-//     if (course[courseName]) {
-//         console.log(course[courseName]);
-//         return course[courseName];
-//     }
-// }
 
-// courseFinder("English")
+// )
+
+
+// // const course = {
+
+// //     Chemistry: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110002.pdf",
+// //     English: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110002.pdf",
+// //     Programming_for_Problem_Solving: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110003.pdf",
+// //     Basic_Electrical_Engineering: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110005.pdf",
+// //     Basic_Mechanical_Engineering: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110006.pdf",
+// //     Environmental_Sciences: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110007.pdf",
+// //     Workshop_Manufacturing_Practices: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110012.pdf",
+// //     Engineering_Graphics_Design: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110013.pdf ",
+// //     Mathematics_1: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110014.pdf",
+// //     Mathematics_2: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110015.pdf",
+// //     Basic_Electronics: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110016.pdf",
+// //     Induction_Program: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110017.pdf",
+// //     Physics: "https://s3-ap-southeast-1.amazonaws.com/gtusitecirculars/Syallbus/3110018.pdf"
+
+
+// // }
+
+// // let courseFinder = (courseName) => {
+// //     if (course[courseName]) {
+// //         console.log(course[courseName]);
+// //         return course[courseName];
+// //     }
+// // }
+
+// // courseFinder("English")
